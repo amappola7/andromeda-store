378c4ad7efd8820ea5ced3c38b45a9cbe96fa317
Amappola <anam.porrasp@gmail.com>
2024-01-18 10:03:03 -0500

    Showing products in home

--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductM } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  productsList: ProductM[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts()
+    .subscribe((products) => {
+      this.productsList = products;
+    })
+  }
 }
