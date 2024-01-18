62bc839b54b5618da83b0cb2ffb48a5cd659a8e8
Amappola <anam.porrasp@gmail.com>
2024-01-18 09:41:12 -0500

    Creating the home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './ui/pages/product-details/product-details.component';
+import { HomeComponent } from './ui/pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'products/:id',
     component: ProductDetailsComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './ui/pages/product-details/product-details.component';
+import { HomeComponent } from './ui/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
