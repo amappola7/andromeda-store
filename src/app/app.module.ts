4f4ea55601342683a3bb03f605c65c92294507f3
Amappola <anam.porrasp@gmail.com>
2024-01-18 09:26:18 -0500

    Creating product details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './ui/pages/products/products.component';
+import { ProductDetailsComponent } from './ui/pages/product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
