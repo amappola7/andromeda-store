a1c8bc4b760c98c41760247a00f318d51fd0f444
Amappola <anam.porrasp@gmail.com>
2024-01-18 09:31:57 -0500

    Creating requests

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, take } from 'rxjs';
+import { ProductM } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private _url: string = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<ProductM[]> {
+    return this.http.get<ProductM[]>(this._url).pipe(
+      take(1),
+      catchError((error) => {
+        console.log('Error getting products', error);
+        return of([]);
+      })
+    )
+  }
+
+  getProductById(id: number): Observable<ProductM | null> {
+    return this.http.get<ProductM>(`${this._url}/${id}`).pipe(
+      take(1),
+      catchError((error) => {
+        console.log('Error getting product', error);
+        return of(null);
+      })
+    )
+  }
 }
