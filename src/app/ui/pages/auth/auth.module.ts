07c9ffcfb7365f505ee3e4f1b1160500cba311cd
Amappola <anam.porrasp@gmail.com>
2024-01-23 11:35:01 -0500

    Creating log in and sign up components

--- src/app/ui/pages/auth/auth.module.ts
+++ src/app/ui/pages/auth/auth.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
+  
+    LogInComponent,
+       SignUpComponent
   ],
   imports: [
     CommonModule,
