6fffb6889ff6fdf51748f2aea9597b3bfa10d28c
Amappola <anam.porrasp@gmail.com>
2024-01-23 10:45:57 -0500

    Displaying home

--- src/app/ui/pages/home/home-routing.module.ts
+++ src/app/ui/pages/home/home-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-const homePaths: Routes = [];
+const homePaths: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(homePaths)],
