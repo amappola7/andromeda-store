753a498a300ba3acf41c36b47c2796b0de1b60a1
Amappola <anam.porrasp@gmail.com>
2024-01-23 21:10:37 -0500

    Creating log in form

--- src/app/ui/pages/auth/components/log-in-form/log-in-form.component.ts
+++ src/app/ui/pages/auth/components/log-in-form/log-in-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in-form',
   templateUrl: './log-in-form.component.html',
   styleUrls: ['./log-in-form.component.scss']
 })
-export class LogInFormComponent {
+export class LogInFormComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {};
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  };
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      this.loginForm.reset();
+    } else {
+      console.log('Invalid form', this.loginForm.value);
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
