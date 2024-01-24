2117aa228465c8255ba9eec79ccca66f9165dcc6
Amappola <anam.porrasp@gmail.com>
2024-01-23 20:14:10 -0500

    Creating form components

--- src/app/ui/pages/auth/auth.module.ts
+++ src/app/ui/pages/auth/auth.module.ts
@@ -4,12 +4,16 @@ import { SharedModule } from '../../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInFormComponent } from './components/log-in-form/log-in-form.component';
+import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
   
     LogInComponent,
-       SignUpComponent
+       SignUpComponent,
+       LogInFormComponent,
+       SignUpFormComponent
   ],
   imports: [
     CommonModule,
