f085ab7f0f12dc1d10a23432a0da02c2f59f716b
Amappola <anam.porrasp@gmail.com>
2024-01-29 11:13:33 -0500

    Refactoring login function

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,7 +25,7 @@ export class AuthService {
   logIn(userData: UserM): void {
     this.userService.getUsers().pipe(
       map((usersList) => usersList.find((user) => userData.username === user.username && userData.password === user.password)),
-      tap((user) => user ? this.setAccessToken() : console.log('Invalid Login'))
+      tap((user) => {if(user) this.setAccessToken()})
     );
   }
 
