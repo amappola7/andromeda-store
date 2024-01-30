7643949d36adf80c0f11b4497ca8e80a5bb0c8b3
Amappola <anam.porrasp@gmail.com>
2024-01-30 07:34:13 -0500

    Creating products table

--- src/app/ui/pages/admin/components/admin-products-table/admin-products-table.component.ts
+++ src/app/ui/pages/admin/components/admin-products-table/admin-products-table.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductM } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-admin-products-table',
   templateUrl: './admin-products-table.component.html',
   styleUrls: ['./admin-products-table.component.scss']
 })
-export class AdminProductsTableComponent {
+export class AdminProductsTableComponent implements OnInit {
+  productList$!: Observable<ProductM[]>;
+
+  constructor(
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.productList$ = this.productService.getProducts();
+  }
 
 }
