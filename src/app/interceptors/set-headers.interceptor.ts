43db881a0ed02157015d64b285e41d2fc3f70b79
Amappola <anam.porrasp@gmail.com>
2024-02-01 12:00:55 -0500

    Creating implementatioin of the set headers interceptor

--- src/app/interceptors/set-headers.interceptor.ts
+++ src/app/interceptors/set-headers.interceptor.ts
@@ -3,16 +3,23 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpHeaders
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class SetHeadersInterceptor implements HttpInterceptor {
-
-  constructor() {}
-
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const token = sessionStorage.getItem('AS_access-token');
+    const headers: HttpHeaders = new HttpHeaders({
+      'content-type': 'application/json',
+      'authorization': `Bearer ${token}`
+    });
+    const newRequest = request.clone({
+      headers: headers
+    })
+    return next.handle(newRequest);
   }
 }
