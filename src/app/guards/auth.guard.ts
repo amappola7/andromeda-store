71ae9f181aa385e61f422fc0cf0d01d77e39a4e5
Amappola <anam.porrasp@gmail.com>
2024-02-01 10:36:05 -0500

    Updating guard behaviour

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,6 +8,9 @@ export const authGuard: CanActivateFn = (route, state) => {
 
   if (authService.userIsAuthenticated() && authService.getUserRole() === 'admin') {
     return true;
+  } else if (authService.getUserRole() === 'user') {
+    alert('You don\'t have access to this page');
+    return router.createUrlTree(['home']);
   } else {
     return router.createUrlTree(['auth/login']);
   }
